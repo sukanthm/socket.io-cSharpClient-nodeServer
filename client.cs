7d62d1c36c45b4d6805aa54bd8e2e230f8825d4b
sukanthm <sukanthm@yahoo.com>
2018-09-29 21:19:27 +0530

    Update client.cs

--- client.cs
+++ client.cs
@@ -3,7 +3,6 @@ using Quobject.SocketIoClientDotNet.Client;
 using System.IO;
 using System.Threading.Tasks;
 using System.Diagnostics;
-using System.Text.RegularExpressions;
 
 namespace ConsoleApp1
 {
@@ -11,13 +10,15 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string ip = "35.237.10.239";
-            string watchPath = @"C:\Users\sukanthk\Downloads\outbox\";
+            bool ssl = true;
+            string ip = "35.190.137.55";
+            string uri = "http://" + ip + ":443";
+            string watchPath = @"D:\Downloads\outbox\";
             System.IO.Directory.CreateDirectory(watchPath);
 
-            if (File.Exists(watchPath + @"client.cs.txt"))
-                File.Delete(watchPath + @"client.cs.txt");
-            File.Copy(@"C:\Users\sukanthk\source\repos\ConsoleApp1\ConsoleApp1\Program.cs", watchPath + @"client.cs.txt"); //latest client code to server
+            if (File.Exists(watchPath + @"client.cs"))
+                File.Delete(watchPath + @"client.cs");
+            File.Copy(@"C:\Users\sukanth\source\repos\ConsoleApp1\ConsoleApp1\Program.cs", watchPath + @"client.cs"); //latest client code to server
 
             int counter = 0;
             string authKey = "a1s2d3f4";
@@ -28,42 +29,53 @@ namespace ConsoleApp1
             {
                 AutoConnect = true,
                 Reconnection = true,
-                ReconnectionDelay = 1000,
-                ReconnectionDelayMax = 5000,
+                ReconnectionDelay = 1*1000,
+                ReconnectionDelayMax = 5*1000,
                 ReconnectionAttempts = int.MaxValue,
+                Timeout = 60*1000,
             };
-            var socket = IO.Socket("http://"+ip+":3000", options);
+
+            if (ssl == true)
+            {
+                options.Secure = true;
+                options.IgnoreServerCertificateValidation = true;
+                uri = uri.Replace("http", "https");
+            }
+
+            var socket = IO.Socket(uri, options);
 
             socket.On(Socket.EVENT_CONNECT, () =>
             {
 
-            if (counter == 0) { Console.WriteLine("socket connected"); counter += 1; }
-            else { Console.WriteLine("socket reconnected"); }
+                if (counter == 0) { Console.WriteLine("socket connected"); counter += 1; }
+                else { Console.WriteLine("socket reconnected"); }
 
-            socket.Emit("join", "{\"hostName\": \"" + System.Net.Dns.GetHostName() + "\", \"authKey\": \"" + authKey + "\"}");
+                socket.Emit("join", "{\"hostName\": \"" + System.Net.Dns.GetHostName() + "\", \"authKey\": \"" + authKey + "\"}");
 
-            FileSystemWatcher watcher = new FileSystemWatcher
-            {
-                Path = watchPath,
-                NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.CreationTime,
-                Filter = "*"
-            };
-                watcher.Created += new FileSystemEventHandler(OnChanged);               
+                FileSystemWatcher watcher = new FileSystemWatcher
+                {
+                    Path = watchPath,
+                    NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.CreationTime,
+                    Filter = "*"
+                };
+                watcher.Created += new FileSystemEventHandler(OnChanged);
 
                 void pushExistingFiles()
                 {
+                    if (counter == 1) { Task.Delay(5 * 1000).Wait(); counter += 1; }
                     string[] files = Directory.GetFiles(watchPath);
                     foreach (string file in files)
                     {
                         string fileName = file.Replace(watchPath, "");
-                        Console.WriteLine("File : " + fileName + " found");
+                        Console.WriteLine("File : " + fileName + " : Found, waiting to get read lock");
                         var byteArray = new byte[] { };
+                        Task.Delay(2 * 1000).Wait(); //wait to get read lock
                         try
                         {
                             byteArray = File.ReadAllBytes(file);
-                            //Console.WriteLine("sending "+ fileName + " to master...");
+                            Console.WriteLine("File : "+ fileName + " : sending to master...");
                             socket.Emit("fromClient", fileName, byteArray);
-                            Console.WriteLine("sent " + fileName + " to master");
+                            //Console.WriteLine("sent " + fileName + " to master");
                         }
                         catch (Exception e)
                         {
@@ -78,21 +90,24 @@ namespace ConsoleApp1
 
                 void OnChanged(object source, FileSystemEventArgs e)
                 {
-                    Console.WriteLine("File : " + e.Name + " " + e.ChangeType);
+                    /*
+                    Console.WriteLine("File : " + e.Name + " " + e.ChangeType+ " : waiting to get read lock");
                     var byteArray = new byte[] { };
+                    Task.Delay(3 * 1000).Wait(); //wait to get read lock
                     try
                     {
                         byteArray = File.ReadAllBytes(e.FullPath);
-                        //Console.WriteLine("sending " + e.Name + " to master...");
+                        Console.WriteLine("sending " + e.Name + " to master...");
                         socket.Emit("fromClient", e.Name, byteArray);
-                        Console.WriteLine("sent " + e.Name + " to master");
+                        //Console.WriteLine("sent " + e.Name + " to master");
                     }
                     catch (Exception e2)
                     {
                         //Console.WriteLine(e2);
                         Console.WriteLine(e.Name + " unreadable.. trying again soon");
                     }
-                    System.Threading.Thread.Sleep(5*1000);
+                    Task.Delay(10 * 1000).Wait();
+                    */
                     pushExistingFiles();
                 }
             });
@@ -101,8 +116,8 @@ namespace ConsoleApp1
             {
                 Console.WriteLine("From server file acknowledgement : " + data);
                 string fileName = data.ToString().Replace(" received", "");
-                File.Delete(watchPath+fileName);
-                Console.WriteLine(fileName+" deleted");
+                File.Delete(watchPath + fileName);
+                Console.WriteLine("File : "+fileName + " : deleted");
             });
 
             socket.On("fromServer", (data) =>
